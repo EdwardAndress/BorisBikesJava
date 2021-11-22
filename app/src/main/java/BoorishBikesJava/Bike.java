fb3ff307f78adfe05328bddbf05ee78ca316edef
Roi Driscoll <roidriscoll@Rois-MacBook-Pro.local>
2021-11-22 17:55:27 +0100

    Adds some breaking stuff

--- app/src/main/java/BoorishBikesJava/Bike.java
+++ app/src/main/java/BoorishBikesJava/Bike.java
@@ -1,7 +1,23 @@
 package BoorishBikesJava;
 
 public class Bike {
+
+  public Boolean broken;
+
+  public Bike() {
+    this.broken = false;
+  }
+
   public String damage() {
+    this.broken = true;
     return "Oi, what have you done, you plebian?";
+  }
+
+  public Boolean isBroken() {
+    return this.broken;
+  }
+
+  public void repair() {
+    this.broken = false;
   }
 }
--- app/src/test/java/BoorishBikesJava/BikeTest.java
+++ app/src/test/java/BoorishBikesJava/BikeTest.java
@@ -1,13 +1,30 @@
 package BoorishBikesJava;
 
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class BikeTest {
   @Test public void isBoorishWhenBroken() {
     Bike bike = new Bike();
     String boorishness = bike.damage();
+
     assertEquals("Oi, what have you done, you plebian?", boorishness);
+  }
+
+  @Test public void isBikeBoorishlyBroken() {
+    Bike bike = new Bike();
+    bike.damage();
+
+    assertTrue(bike.isBroken());
+  }
+
+  @Test public void isBikeRudelyRepaired() {
+    Bike bike = new Bike();
+    bike.damage();
+    bike.repair();
+
+    assertFalse(bike.isBroken());
   }
 }
